bda72aeb3084c4d855c10f6eaf81cc83494d3e60
martechsv <enzo@postland.io>
2020-02-10 23:41:21 -0800

    this message

---
+++ artist.py
@@ -0,0 +1,26 @@
+from MSG_Installer import Installatiof
+
+Install = Installation()
+print('\nPerforming an artist installation...')
+
+# APPLICATIONS
+Install.creative_cloud()
+Install.nuke()
+Install.c4d()
+Install.deadline()
+Install.quicktime()
+Install.anydesk()
+Install.splunk()
+
+# PLUGINS
+Install.optical_flares()
+Install.re_vision()
+Install.neat_video()
+Install._7th_sense()
+Install.red_giant()
+Install.ft_uvpass()
+Install.octane()
+Install.x_particles()
+
+# DRIVERS
+# Install.nvidia_driver()
